5cc444d64408a2fa80514aa5151305c0c9e83983
Osama <osamazaheer057@gmail.com>
2025-01-06 13:18:15 +0530

    added get method to read all data

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI
+from typing import Annotated
+from sqlalchemy.orm import Session
+from fastapi import Depends, FastAPI
 import models
-from database import engine
+from models import Todos
+from database import engine, SessionLocal
 
 app = FastAPI()
 
@@ -8,3 +11,17 @@ app = FastAPI()
 # Generates the necessary database schema based on the ORM models defined in models.py.
 # Creates the todos table in the SQLite database (todos.db) if it does not already exist.
 models.Base.metadata.create_all(bind= engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@app.get("/")
+# Depends is dependency injection
+async def read_all(db: db_dependency):
+    return db.query(Todos).all()
